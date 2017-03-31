d02d2d9d125499f25589d3a88c0146a8e1313eb7
kurtis <davidsku18@up.edu>
2017-03-30 21:24:38 -0700

    Finished Lab 08

--- app/src/main/java/edu/up/cs371/soccer_application/SoccerDatabase.java
+++ app/src/main/java/edu/up/cs371/soccer_application/SoccerDatabase.java
@@ -8,8 +8,6 @@ import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.util.*;
 
